5240f9b4faea811e773434d44d8afb3650b87085
sumit <sk24csb0b76@student.nitw.ac.in>
2026-07-01 18:39:54 +0530

    fix: update API base URL to production backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: 'https://gearrent-backend.onrender.com'
 })
 
 api.interceptors.request.use((config) => {
